cfdb188f2d419ce29a2dd12bb4f886786b86b159
Yumenio <alejandroandy447@gmail.com>
2024-02-18 12:48:00 -0500

    feat: generates account_id and account_number for new accounts by overriding the model.save() method

--- gpa/api/models/account.py
+++ gpa/api/models/account.py
@@ -12,3 +12,23 @@ class Account(models.Model):
 
     def __str__(self):
         return f"{self.account_id} - {self.account_number}"
+
+    def save(self, *args, **kwargs):
+        if not self.account_id:
+            latest_account = Account.objects.order_by("-ID").first()
+            if latest_account and latest_account.account_id:
+                last_id = int(latest_account.account_id.split("-")[1])
+                self.account_id = "A-{0:04d}".format(last_id + 1)
+            else:
+                self.account_id = "A-0001"
+
+        if not self.account_number:
+            # Generate account_number if not provided
+            latest_account = Account.objects.order_by("-ID").first()
+            if latest_account and latest_account.account_number:
+                last_number = int(latest_account.account_number.replace(" ", ""))
+                self.account_number = "{:016d}".format(last_number + 1)
+            else:
+                self.account_number = "0000000000000001"
+
+        super(Account, self).save(*args, **kwargs)
