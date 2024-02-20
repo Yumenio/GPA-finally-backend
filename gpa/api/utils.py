eeb5f276652bd1fd0ac51bdd675ccd7a324c4c8f
Yumenio <alejandroandy447@gmail.com>
2024-02-19 20:32:40 -0500

    feat: adds AuthBearer for decoding JwtToken in requests

--- gpa/api/utils.py
+++ gpa/api/utils.py
@@ -1,4 +1,7 @@
+from jwt import decode
 from itertools import chain
+from django.conf import settings
+from ninja.security import HttpBearer
 
 
 def to_dict(instance):
@@ -9,3 +12,9 @@ def to_dict(instance):
     for f in opts.many_to_many:
         data[f.name] = [i.id for i in f.value_from_object(instance)]
     return data
+
+
+class AuthBearer(HttpBearer):
+    def authenticate(self, request, token):
+        print("token", token)
+        return decode(token, settings.SECRET_KEY, "HS256")
